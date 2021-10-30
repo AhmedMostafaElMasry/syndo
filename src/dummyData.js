eada7b6dae85543146c36134c9016b7fe9760bb2
Ahmed <a.mostafa.elmasry@gmail.com>
2021-10-30 17:53:34 +0200

    create data file

---
+++ src/dummyData.js
@@ -0,0 +1,86 @@
+export const ExplorePosts = [
+  {
+    id: 1,
+    image: "assets/resala.png",
+    title: "Resala",
+    tillDate: "Till 11/10/2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolores, numquam dolore aperiam, nulla velit, pariatur fugit voluptatem eligendi commodi vero excepturi qui beatae nihil provident!",
+    percentage: "60%",
+    FundingGoal: "1,000,000",
+    IntersetRate: "12%",
+    Tenor: "12 months",
+    TotalInvested: "EGP 500,000",
+    Investor: "5",
+    SkinInTheGame: "5%",
+  },
+  {
+    id: 2,
+    image: "assets/ahl-masr.jfif",
+    title: "Ahl Masr",
+    tillDate: "Till 11/10/2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolores, numquam dolore aperiam, nulla velit, pariatur fugit voluptatem eligendi commodi vero excepturi qui beatae nihil provident!",
+    percentage: "80%",
+    FundingGoal: "1,000,000",
+    IntersetRate: "12%",
+    Tenor: "12 months",
+    TotalInvested: "EGP 500,000",
+    Investor: "5",
+    SkinInTheGame: "5%",
+  },
+  {
+    id: 3,
+    image: "assets/resala.png",
+    title: "Resala",
+    tillDate: "Till 11/10/2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolores, numquam dolore aperiam, nulla velit, pariatur fugit voluptatem eligendi commodi vero excepturi qui beatae nihil provident!",
+    percentage: "60%",
+    FundingGoal: "1,000,000",
+    IntersetRate: "12%",
+    Tenor: "12 months",
+    TotalInvested: "EGP 500,000",
+    Investor: "5",
+    SkinInTheGame: "5%",
+  },
+  {
+    id: 4,
+    image: "assets/ahl-masr.jfif",
+    title: "Ahl Masr",
+    tillDate: "Till 11/10/2021",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolores, numquam dolore aperiam, nulla velit, pariatur fugit voluptatem eligendi commodi vero excepturi qui beatae nihil provident!",
+    percentage: "80%",
+    FundingGoal: "1,000,000",
+    IntersetRate: "12%",
+    Tenor: "12 months",
+    TotalInvested: "EGP 500,000",
+    Investor: "5",
+    SkinInTheGame: "5%",
+  },
+];
+
+export const Doers = [
+  {
+    id: 1,
+    pragraph: '"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate voluptate officiis"',
+    image: "",
+    name: "Jane Deo",
+    position: "company XYZ-CEO",
+  },
+  {
+    id: 2,
+    pragraph: '"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate voluptate officiis"',
+    image: "",
+    name: "Jane Deo",
+    position: "company XYZ-CEO",
+  },
+  {
+    id: 3,
+    pragraph: '"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate voluptate officiis"',
+    image: "",
+    name: "Jane Deo",
+    position: "company XYZ-CEO",
+  },
+]
